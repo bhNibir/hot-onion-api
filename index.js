993c650a4a68ee9b40d0e744eb0e46ece745330f
nibir <biplob.asanibir@gmail.com>
2020-04-05 01:53:57 +0600

    add quary by key

--- index.js
+++ index.js
@@ -1,7 +1,81 @@
 const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 4200
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//Database
+const MongoClient = require('mongodb').MongoClient;
+const uri =  process.env.DB_PATH
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+
+//Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => res.send('Hello Server is running!'))
+
+app.post('/addItems', (req, res) => {
+    const items = req.body
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hot-onion").collection("foodItems");
+    collection.insert(items, (err, result) => {
+        if(err){
+            console.log(err);
+            res.status(500).send({message:err})
+            
+        }
+        else{
+            res.send(result.ops[0])
+        }
+    })
+    client.close();
+    });
+})
+
+
+app.get('/foodItems', (req, res) => {
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hot-onion").collection("foodItems");
+    collection.find().toArray((err, documents) => {
+        if(err){
+            console.log(err);
+            res.status(500).send({message:err})
+            
+        }
+        else{
+            res.send(documents)
+        }
+    })
+    client.close();
+    });
+})
+
+app.get('/foodItems/:key', (req, res) => {
+    const key = req.params.key
+    console.log(key)
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("hot-onion").collection("foodItems");
+    collection.find({key}).toArray((err, documents) => {
+        if(err){
+            console.log(err);
+            res.status(500).send({message:err})
+            
+        }
+        else{
+            res.send(documents)
+        }
+    })
+    client.close();
+    });
+})
 
 app.listen(port, () => console.log(` Server listening at port: ${port}`))
